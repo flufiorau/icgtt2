0e8520e0f9c3571701bcccdfa72900bd8eafaf4a
flufiorau <flufiorau@ukr.net>
2018-04-03 22:57:18 +0300

    update client component / create filter functional

--- src/app/clients/clients.component.ts
+++ src/app/clients/clients.component.ts
@@ -11,6 +11,7 @@ import {HttpService} from '../http.service';
 export class ClientsComponent implements OnInit {
 
     clients: Client;
+    selectedClient: Client;
 
     constructor(private httpService: HttpService) {
     }
@@ -20,4 +21,32 @@ export class ClientsComponent implements OnInit {
             .subscribe((data: Client) => this.clients = data);
     }
 
+    onFilter(event) {
+        const filterValue = event.target.value.toLowerCase();
+
+        this.httpService.getData()
+            .subscribe((data: any) => {
+                const filteredClients = data.filter(findAnalogyInObject(filterValue));
+
+                function findAnalogyInObject(value) {
+                    const finder = function (obj) {
+                        for (const key in obj) {
+                            if (typeof(obj[key]) === 'string') {
+                                const someValue = obj[key].toLowerCase();
+                                if (someValue.indexOf(value) !== -1) {
+                                    return true;
+                                }
+                            }
+                            if (typeof(obj[key]) === 'object' && finder(obj[key]) === true) {
+                                return true;
+                            }
+                        }
+                        return false;
+                    };
+                    return finder;
+                }
+
+                this.clients = filteredClients;
+            });
+    }
 }
